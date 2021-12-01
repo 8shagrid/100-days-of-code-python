d9f7f451e8205877a1d91304756f7a27a5868bb1
Dirga <86204615+8shagrid@users.noreply.github.com>
2021-12-01 17:47:42 +0700

    Delete projects/day01 directory

--- projects/day01/main.py
+++
@@ -1,7 +0,0 @@
-print("Band Name Generator")
-
-kota = input("Apa nama kota tempat Anda dibesarkan?\n")
-
-hewan_peliharaan = input("Siapa nama hewan peliharaanmu?\n")
-
-print(f"Nama band kamu '{hewan_peliharaan} {kota}'")
