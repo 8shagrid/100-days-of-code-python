31dca548dc0ae94e961ed9cd467fe36c2d0d771d
Dirga <86204615+8shagrid@users.noreply.github.com>
2021-12-02 18:47:00 +0700

    Add files via upload

---
+++ day05/main.py
@@ -0,0 +1,45 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Password Generator")
+nr_letters = int(input("Berapa banyak huruf yang Kamu inginkan?\n> ")) 
+nr_symbols = int(input(f"Berapa banyak simbol yang Kamu inginkan?\n> "))
+nr_numbers = int(input(f"Berapa banyak angka yang Kamu inginkan?\n> "))
+
+# Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# print(password)
+
+#Hard Level
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+# print(password_list)
+random.shuffle(password_list)
+# print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Kata sandi Kamu adalah: {password}")
